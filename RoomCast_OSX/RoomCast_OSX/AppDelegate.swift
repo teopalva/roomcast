0cf39f2b98756da1eef7afb5c7b961a3848ce5f8
Matteo Palvarini <teo.palvarini@gmail.com>
2015-01-19 14:49:56 -0600

    Added OS X project

---
+++ RoomCast_OSX/RoomCast_OSX/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  RoomCast_OSX
+//
+//  Created by Matteo Palvarini on 19/01/15.
+//  Copyright (c) 2015 Matteo Palvarini. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
