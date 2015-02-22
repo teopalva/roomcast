93e19feb968ee33867411aea5eee388156647a4c
Matteo Palvarini <teo.palvarini@gmail.com>
2015-02-22 17:07:08 -0600

    Added first implementation of custom url scheme

--- RoomCast_iPad/MenuViewController.swift
+++ RoomCast_iPad/MenuViewController.swift
@@ -7,9 +7,10 @@
 //
 
 import UIKit
+import Foundation;
 import Nutella
 
-class MenuViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+class MenuViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout, UIGestureRecognizerDelegate {
     
     var channels = [Channel]()
     
@@ -20,7 +21,7 @@ class MenuViewController: UICollectionViewController, UICollectionViewDelegateFl
         self.navigationController?.setNavigationBarHidden(true, animated: false)
         self.navigationController?.interactivePopGestureRecognizer.enabled = true
         self.navigationController?.interactivePopGestureRecognizer.delegate = nil
-
+        
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
         
@@ -32,12 +33,12 @@ class MenuViewController: UICollectionViewController, UICollectionViewDelegateFl
         //var nutella = Nutella(host: "10.0.0.3", clientId: nil)
         //nutella.syncRequest("mapping/update")
         
-        /*
+        
         let ch1 = Channel(name:"Channel1", iconName: "ch01", description: "The first channel", contentUrl: "http://apple.com")
         channels.append(ch1)
         let ch2 = Channel(name:"Channel2", iconName: "ch02", description: "The second channel", contentUrl: "http://google.com")
         channels.append(ch2)
-        let ch3 = Channel(name:"Channel3", iconName: "ch03", description: "The third channel", contentUrl: nil)
+        let ch3 = Channel(name:"Channel3", iconName: "ch03", description: "The third channel", contentUrl: "roomquake.seismometer://?param1=value")
         channels.append(ch3)
         let ch4 = Channel(name:"Channel4", iconName: "ch04", description: "The fourth channel", contentUrl: "http://mpalva2.people.uic.edu/viz")
         channels.append(ch4)
@@ -47,7 +48,7 @@ class MenuViewController: UICollectionViewController, UICollectionViewDelegateFl
             let ch = Channel(name:"Channel", iconName: "ch00", description: "Another channel", contentUrl: nil)
             channels.append(ch)
         }
-        */
+        
         
     }
     
@@ -77,15 +78,43 @@ class MenuViewController: UICollectionViewController, UICollectionViewDelegateFl
         ch.imageView.image = image1
         ch.imageView.setCornerRadius()
         
-        var tgr = UITapGestureRecognizer(target:self, action:Selector("imageTapped:"))
-        ch.imageView.addGestureRecognizer(tgr)
+        // add action to clicked imageView
+        //var recognizer = UITapGestureRecognizer(target:self, action:Selector("imageTapped:"))
+        //recognizer.delegate = self
+        //ch.addGestureRecognizer(recognizer)
         
         return ch
         
     }
     
-    func imageTapped(img: AnyObject) {
-        self.performSegueWithIdentifier("webviewSegue", sender: self)
+    /* handles click on imageView
+    func imageTapped(recognizer: UITapGestureRecognizer) {
+    println("tap")
+    self.performSegueWithIdentifier("webviewSegue", sender: self)
+    }
+    */
+    
+    /**
+    * Handles tap on a channel. Shows either a web or native content.
+    */
+    override func collectionView(collectionView: UICollectionView, didSelectItemAtIndexPath indexPath: NSIndexPath) {
+        
+        var sender_ = self.collectionView!.cellForItemAtIndexPath(indexPath)
+        var url: String? = self.channels[indexPath.item].contentUrl
+        if let url = url {
+            if (url.substringWithRange(Range<String.Index>(start: url.startIndex, end: advance(url.startIndex, 7))) == "http://") {
+                self.performSegueWithIdentifier("webviewSegue", sender: sender_)
+            } else {
+                let customUrl: NSURL = NSURL(string:url)!
+                if (UIApplication.sharedApplication().canOpenURL(customUrl)) {
+                    UIApplication.sharedApplication().openURL(customUrl)
+                } else {
+                    println("Could not open " + url)
+                }
+            }
+        } else {
+            println("Empty Channel!")
+        }
     }
     
     // MARK: UICollectionViewDelegate
@@ -136,6 +165,8 @@ class MenuViewController: UICollectionViewController, UICollectionViewDelegateFl
     // MARK: - Navigation
     
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        
+        println("called prepare:", sender)
         
         // Get the new view controller
         var nextVC = segue.destinationViewController as ChannelViewController
