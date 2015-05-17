1c32d2c2ae87318f00dd6f9d6bfa8e7b90ede63b
Matteo Palvarini <teo.palvarini@gmail.com>
2015-05-16 23:28:50 -0500

    Adding login support to iPad

---
+++ RoomCast_iPad_webapp/RoomCast_iPad_webapp/LoginViewController.swift
@@ -0,0 +1,9 @@
+//
+//  LoginViewController.swift
+//  RoomCast_iPad_webapp
+//
+//  Created by Matteo Palvarini on 16/05/15.
+//  Copyright (c) 2015 Matteo Palvarini. All rights reserved.
+//
+
+import Foundation
