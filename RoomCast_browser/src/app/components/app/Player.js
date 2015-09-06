cf173dd1dc742ffe0292f66459fe46653148b4a8
Matteo Palvarini <teo.palvarini@gmail.com>
2015-09-06 18:19:41 -0500

    Added loading

--- RoomCast_browser/src/app/components/app/Player.js
+++ RoomCast_browser/src/app/components/app/Player.js
@@ -2,11 +2,12 @@
 var React = require('react');
 var Mui = require('material-ui');
 var FloatingActionButton = Mui.FloatingActionButton;
+var d3 = require('d3');
 
 var Player = React.createClass({
 
     componentDidMount: function() {
-
+        this.showLoading();
     },
 
     componentWillUnmount: function() {
@@ -15,12 +16,73 @@ var Player = React.createClass({
 
     getInitialState: function () {
         return  {
-            playing: true
+            playing: true,
+            loading: true
         }
     },
 
     componentWillReceiveProps: function(props) {
         this.setState({playing: props.playing});
+    },
+
+    handleOnLoad: function() {
+        this.setState({loading: false});
+        this.hideLoading();
+    },
+
+    showLoading: function() {
+        var self = this;
+
+        d3.xml("assets/Logo_grid.svg", "image/svg+xml", function (logo) {
+
+            self.refs['player'].getDOMNode().appendChild(logo.documentElement);
+
+            d3.select('#logo')
+                .attr({
+                    width: window.innerWidth,
+                    height: window.innerHeight,
+                });
+
+            var colorBlue = function(id) {
+                d3.select(id).style({
+                    'fill': '#00bcd4'
+                });
+            };
+
+            var colorPink = function(id) {
+                d3.select(id).style({
+                    'fill': '#e91e63'
+                });
+            };
+
+            var removeColor = function() {
+                d3.selectAll('rect').style({
+                    'fill': null
+                });
+            };
+
+            var sequence = [1,2,3,4,8,7,6,5];
+            var i = 0;
+            var action = function() {
+                removeColor();
+                if(sequence[i] < 5) {
+                    colorBlue('#ch' + sequence[i]);
+                } else {
+                    colorPink('#ch' + sequence[i]);
+                }
+                i++;
+                if(i===sequence.length) {
+                    i = 0;
+                }
+            };
+
+            setInterval(action, 200);
+
+        });
+    },
+
+    hideLoading: function() {
+        d3.select('#logo').remove();
     },
 
     render: function () {
@@ -49,7 +111,7 @@ var Player = React.createClass({
 
         return (
 
-            <div className='player' style={playerStyle} >
+            <div className='player' style={playerStyle} ref='player' >
 
                 <div className='back-button' style={buttonStyle} >
                     <FloatingActionButton
@@ -62,7 +124,7 @@ var Player = React.createClass({
                     </FloatingActionButton>
                 </div>
 
-                <iframe className='channel-frame' src={this.props.url} > </iframe>
+                <iframe className='channel-frame' src={this.props.url} onload={this.handleOnLoad} > </iframe>
 
             </div>
 
