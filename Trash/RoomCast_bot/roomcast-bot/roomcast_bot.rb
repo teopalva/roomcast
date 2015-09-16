6900556d12830ad172b0adebdd39888c0b6b6997
Matteo Palvarini <teo.palvarini@gmail.com>
2015-09-16 00:46:13 -0500

    Cleaned repo

--- Trash/RoomCast_bot/roomcast-bot/roomcast_bot.rb
+++
@@ -1,9 +0,0 @@
-require 'nutella_lib'
-require 'net/http'
-require 'net/https'
-require 'uri'
-require 'json'
-
-
-# Initialize nutella
-nutella.init ARGV
--- Trash/RoomCast_bot/roomcast-bot/startup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-BASEDIR=$(dirname $0)
-ruby $BASEDIR/roomcast_bot.rb $1 $2
